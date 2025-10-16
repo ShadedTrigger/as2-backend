2b45f5f5231cf5daa68dde5e29b4ba68cf5a3d0f
Sean <seanjasminsnelson@gmail.com>
2025-10-15 20:01:05 -0400

    Fixed routing in server.js

--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const contactsRoute = require("../server/routes/contactsRoute");
 const educationsRoute = require("../server/routes/educationsRoute");
+const projectsRoute = require("../server/routes/projectsRoute");
+const usersRoute = require("../server/routes/usersRoute");
 dotenv.config();
 
 const app = express();
@@ -21,6 +23,8 @@ app.get("/", (req, res) => {
 
 app.use("/contacts", contactsRoute);
 app.use("/educations", educationsRoute);
+app.use("/projects", projectsRoute);
+app.use("/users", usersRoute);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("API is up at " + (process.env.PORT || 3000));
